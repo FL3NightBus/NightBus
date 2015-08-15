5fc53d59208e53a0de50c6bb8c299d48e9c7233d
Nastya Sokolova <nas92sokol@gmail.com>
2015-08-15 04:16:07 +0300

    improving draw and hide functions

--- dev/js/polyline.js
+++ dev/js/polyline.js
@@ -1,26 +1,25 @@
-
 /*==== View of map ===*/
 var MapView = Backbone.View.extend({
   id: 'map-canvas',
 
-  initialize: function(){
+  initialize: function() {
     var mapOptions = {
-        center: {
-          lat: 49.80485402,
-          lng: 23.98729831
-        },
-        zoom: 12
-   };
+      center: {
+        lat: 49.80485402,
+        lng: 23.98729831
+      },
+      zoom: 12
+    };
 
     this.map = new google.maps.Map(this.el, mapOptions);
 
     var polyOptions = {
-        strokeOpacity: 0.5,
-        strokeWeight: 5
+      strokeOpacity: 0.7,
+      strokeWeight: 5
     };
 
- 
-   this.poliArray = {
+
+    this.poliArray = {
       '1Н': new google.maps.Polyline(polyOptions),
       '2Н': new google.maps.Polyline(polyOptions),
       '3Н': new google.maps.Polyline(polyOptions),
@@ -29,11 +28,11 @@ var MapView = Backbone.View.extend({
       '6Н': new google.maps.Polyline(polyOptions),
       '7Н': new google.maps.Polyline(polyOptions)
     };
-   
+
     this.render();
   },
 
-  render: function(){
+  render: function() {
     $('#map-canvas').replaceWith(this.el);
   },
 
@@ -47,7 +46,7 @@ var MapView = Backbone.View.extend({
 
 });
 
- mapView = new MapView();
+mapView = new MapView();
 
 
 /*========== View of Cheakboxes ==========*/
@@ -57,123 +56,161 @@ var PoliView = Backbone.View.extend({
   el: '#polylines',
 
   initialize: function() {
-    
+
     this.response = [];
     this.setResponse();
     this.poliarr = mapView.getPoliArray();
   },
-    
-  
+
+
   setResponse: function() {
-   
-     var that = this;
+
+    var that = this;
     $.ajax({
       type: "GET",
       async: true,
-      dataType: 'json', 
-      url: 'http://localhost:8080/api/routes/',  
-      
-     
-      success: function(resp) {  
-         that.response = resp;
-         console.log(this.response)
+      dataType: 'json',
+      url: 'http://localhost:8080/api/routes/',
+
+
+      success: function(resp) {
+        that.response = resp;
+        console.log(this.response)
       },
       error: function(err) {
         console.log(err);
       }
-   })
+    })
   },
 
   getResponse: function() {
-     console.log('getresp: ' + this.response)
+    console.log('getresp: ' + this.response)
     return this.response;
   },
 
 
   getPoli: function(waynumber) {
-  
-  var coords = [];  
 
-  switch(waynumber) {
-    case '1Н': { 
-                coords = this.getCoords(5);
-                this.poliarr[waynumber].setOptions({strokeColor: 'blue', path: coords});
-                break;    
-               }
+    var coords = [];
 
-    case '2Н': {                
-                coords = this.getCoords(6);
-                this.poliarr[waynumber].setOptions({strokeColor: 'green', path: coords});
-                break;     
-              } 
+    switch (waynumber) {
+      case '1Н':
+        {
+          coords = this.getCoords(5);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#1976D2',
+            path: coords
+          });
+          break;
+        }
 
-    case '3Н': { 
-                coords = this.getCoords(0);
-                this.poliarr[waynumber].setOptions({strokeColor: 'black', path: coords});
-                break;
-              }
+      case '2Н':
+        {
+          coords = this.getCoords(6);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#009688',
+            path: coords
+          });
+          break;
+        }
 
-    case '4Н': {  
-                coords = this.getCoords(1);
-                this.poliarr[waynumber].setOptions({strokeColor: 'red', path: coords});
-                break;
-              }
+      case '3Н':
+        {
+          coords = this.getCoords(0);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#303F9F',
+            path: coords
+          });
+          break;
+        }
 
-    case '5Н': {  
-                coords = this.getCoords(2);
-                this.poliarr[waynumber].setOptions({strokeColor: 'purple', path: coords});
-                break;
-              }
+      case '4Н':
+        {
+          coords = this.getCoords(1);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#FF5722',
+            path: coords
+          });
+          break;
+        }
 
-    case '6Н': { 
-                coords = this.getCoords(4);
-                this.poliarr[waynumber].setOptions({strokeColor: 'yellow', path: coords});
-                break;
-              }
+      case '5Н':
+        {
+          coords = this.getCoords(2);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#FFC107',
+            path: coords
+          });
+          break;
+        }
 
-    case '7Н': { 
-                coords = this.getCoords(3);
-                this.poliarr[waynumber].setOptions({strokeColor: 'brown', path: coords});
-                break;
-              }  
-    }                          
-   },
+      case '6Н':
+        {
+          coords = this.getCoords(4);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#C2185B',
+            path: coords
+          });
+          break;
+        }
 
-   getCoords: function(number) {
+      case '7Н':
+        {
+          coords = this.getCoords(3);
+          this.poliarr[waynumber].setOptions({
+            strokeColor: '#4CAF50',
+            path: coords
+          });
+          break;
+        }
+    }
+  },
 
-   
-     var res = this.getResponse();
-     console.log(res)
+  getCoords: function(number) {
+
+    var res = this.getResponse();
+    console.log(res)
     var templ = res[number].routeArray;
     return templ.map(function(el) {
-        return (new google.maps.LatLng(el.lat, el.lng));
+      return (new google.maps.LatLng(el.lat, el.lng));
     })
-   
-   },
 
-   events: {
+  },
+
+  events: {
     'click input:checkbox': 'passPoliline'
-   },
+  },
 
-   passPoliline: function(e) {
+  drawPoliline: function(name) {
+    var map = mapView.getMap();
+    var that = this;
+    $.when(that.getPoli(name)).then(function() {
+      that.poliarr[name].setMap(map);
+    })
+  },
+
+  hidePoliline: function(name) {
+    var map = mapView.getMap();
+    var that = this;
+    $.when(that.getPoli(name)).then(function() {
+      that.poliarr[name].setMap(null);
+    })
+  },
+
+  passPoliline: function(e) {
     var $checkbox = $(e.currentTarget);
-   
-     var map = mapView.getMap();
-     waynumber = ($checkbox).attr('id');
+    var that = this;
+    var map = mapView.getMap();
+    waynumber = ($checkbox).attr('id');
 
-     this.getPoli(waynumber);
-     
-     if ($checkbox.is(':checked')) {
-        this.poliarr[waynumber].setMap(map);
-        
-     } else { 
-   
-        this.poliarr[waynumber].setMap(null);    
-     }
-   }
+    if ($checkbox.is(':checked')) {
+      this.drawPoliline(waynumber);
+    } else {
+      this.hidePoliline(waynumber);
+    }
+  }
 
 })
 
 
-var pv = new PoliView();
-
+var polyline = new PoliView();
