7eace472ca1a5bb4d5a508d897f48a529fe142b1
AndriyShevchuk <andriy.shevchyk@gmail.com>
2015-08-18 02:25:29 +0300

    update template, add styles for mobile onLineTrfficViewPage

--- dev/templates/onLineTrafficViewTemplate.js
+++ dev/templates/onLineTrafficViewTemplate.js
@@ -1,5 +1,6 @@
 var onLineTrafficViewTemplate = '<div class="page onLineTrafficPage">' +
-        '<form action="#" id="form">' +
+        '<div id="form">' +
+          '<p class="header">Вони їздять</p>' +
           '<input class="busInput" type="checkbox" id="1">' +
           '<input class="busInput" type="checkbox" id="2">' +
           '<input class="busInput" type="checkbox" id="3">' +
@@ -7,9 +8,10 @@ var onLineTrafficViewTemplate = '<div class="page onLineTrafficPage">' +
           '<input class="busInput" type="checkbox" id="5">' +
           '<input class="busInput" type="checkbox" id="6">' +
           '<input class="busInput" type="checkbox" id="7">' +
-        '</form>' +
+        '</div>' +
         '<div id="polylines">' +
           '<div id="sp">' +
+            '<p class="header">маршрутами</p>' +
             '<span class="routeSpan">1H</span>' +
             '<span class="routeSpan">2H</span>' +
             '<span class="routeSpan">3H</span>' +
@@ -19,6 +21,7 @@ var onLineTrafficViewTemplate = '<div class="page onLineTrafficPage">' +
             '<span class="routeSpan">7H</span><br>' +
           '</div>' +
           '<div id="pi">' +
+            '<p class="header">ось цими</p>' +
             '<input class="polylineInput" id="1H" type="checkbox">' +
             '<input class="polylineInput" id="2H" type="checkbox">' +
             '<input class="polylineInput" id="3H" type="checkbox">' +
@@ -28,18 +31,27 @@ var onLineTrafficViewTemplate = '<div class="page onLineTrafficPage">' +
             '<input class="polylineInput" id="7H" type="checkbox"><br>' +
           '</div>' +
         '</div>' +
-        '<div id="ftr">Введи інтервал оновлення позиції автобуса (в секундах)' +
-        '<input type="number" placeholder="5" id="interval" min="5" max="30" step="5">' +
-        '<button id="location">Were I am?</button>' +
-        '</div>' +
-        '<div id="busInfo"></div>' +
+        '<div id="ftr">Як часто показувати автобуси ?' +
+            '<input type="number" placeholder="5 сек" id="interval" min="5" max="30" step="5">' +
+            '</div>' +
+            '<button id="location" class="btn btn-default">Готово</button>' +
+        '<div id="busInfo">Нічний маршрут №2Н' +
+            'пл. Різні - Залізничний вокзал - вул. Патона' +
+            'Ціна: 8.00 UAH' +
+            'Інтервал руху: 80 хв.' +
+            'Час: 23:30 - 04:50' +
+            'Прямий маршрут: пл. Різні - Залізничний вокзал - вул. Виговського - Церква Воскресіння Господнього' +
+            'Зворотній маршрут: Церква Воскресіння Господнього - вул. Виговського - вул. Кульпарківська - Залізничний вокзал - пл. Різні</div>' +
       '</div>';
 
-var busInfoTemplate = '<span id="closeInfo">x</span>' +
+var busInfoTemplate = '<script type="tmpl/html" id="info-template">' +
+   '<span id="close"></span>' +
       '<p><%= obj.route%></p>' +
       '<p><%= obj.way %></p>' +
       '<p>Ціна: <%= obj.price %></p>' +
       '<p>Інтервал руху: <%= obj.interval %></p>' +
       '<p>Час: <%= obj.time %></p>' +
       '<p>Прямий маршрут: <%= obj.sw %></p>' +
-      '<p>Зворотній маршрут: <%= obj.bw %></p>';
+      '<p>Зворотній маршрут: <%= obj.bw %></p>' +
+    '</div>' +
+   '</script>';
